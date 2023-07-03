617215063c39e53cfef99eb0de87371181cc53d1
Minjae Chang <minjae.j.chang@gmail.com>
2023-07-03 21:14:35 +1200

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -27,3 +27,15 @@ document.addEventListener('scroll', () => {
     arrowUp.style.opacity = 0;
   }
 });
+
+// Navbar 토글버튼 클릭 처리
+const navbarToggle = document.querySelector('.header__toggle');
+const navbarMenu = document.querySelector('.header__menu');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
+// Navbar menu 클릭시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+});
